fd5e0e5bf4c5cae4d0f5b22ab8cb3cdf936a628a
Alexandre Nogueira <alx.delira@gmail.com>
2025-08-05 16:00:59 -0400

    verificando build com vite complexo de mais

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,2 +1,8 @@
 // worker-configuration.d.ts
 export {};
+
+declare interface Env {
+  // Declare aqui as variáveis de ambiente que você usa no worker
+  MY_SECRET?: string;
+  SOME_KEY?: string;
+}
